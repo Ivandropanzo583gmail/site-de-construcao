4a7e204ff80540cc202f612f8b594255e3516b68
Ivandro Panzo <ivandropanzo583@gmail.com>
2024-09-12 17:03:48 +0100

    js modificado

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/site-de-constru-o',
+  assetPrefix: '/site-de-constru-o/',
+}
+/** @type {import('next').NextConfig} */
+  module.exports = {
+  output: 'export',
 }
